a986096d30d13788134eea100f16cbbfef99b188
Alexandre AZOR <azor.alexandre@gmail.com>
2022-07-20 21:37:08 +0200

    uninteactive title screen

--- src/title.cpp
+++ src/title.cpp
@@ -9,7 +9,7 @@ State title(SDL_Renderer *pRenderer, TTF_Font *pFont)
     SDL_Rect textRects[TITLE_TEXT_NBR_OF_ELEMENTS];
     SDL_Rect puzzleSizesRects[NUMBER_OF_SIZES];
     SDL_Rect imgSDLRect;
-    SDL_Rect selectionRect;
+    SDL_Rect selectionRect = SDL_Rect{0, 0, 0, 0};
 
     SDL_Event events;
     State state = STATE_TITLE;
@@ -55,11 +55,16 @@ int create_textures_title(SDL_Renderer *&pRenderer,
     SDL_Colour fgColour = SDL_Colour{255, 255, 255, 255};
     SDL_Colour bgColour = SDL_Colour{0, 0, 0, 255};
 
-    char *textsArray[] = {"Slide Puzzle",
-                          "Select the mode and choose the size",
-                          "Current mode: Game    Demo",
-                          "In game, press P to highlight the first misplaced bloc",
-                          "Press M to change the mode"};
+    char textTitle[] = {"======Slide Puzzle======"};
+    char textInstruction[] = {"Select the mode and choose the size"};
+    char textMode[] = {"Current mode: Game    Demo"};
+    char textPressP[] = {"P: Highlight the first misplaced bloc"};
+    char *textsArray[] = {textTitle,
+                          textInstruction,
+                          textMode,
+                          textPressP};
+
+    char imgPath[] = {IMG_PATH};
 
     int puzzleSizeInt, i, j;
     char puzzleSizeChar[10];
@@ -69,7 +74,7 @@ int create_textures_title(SDL_Renderer *&pRenderer,
         if (create_text_texture(pTextTextures[i], pFont, textsArray[i], fgColour, bgColour, pRenderer) < 0)
         {
             for (j = 0; j < i; j++)
-                SDL_DestroyTexture(pTextTextures[i]);
+                SDL_DestroyTexture(pTextTextures[j]);
             return -1;
         }
     }
@@ -91,7 +96,7 @@ int create_textures_title(SDL_Renderer *&pRenderer,
         }
     }
 
-    if (create_img_texture(pImgSDLTexture, IMG_PATH, pRenderer) < 0)
+    if (create_img_texture(pImgSDLTexture, imgPath, pRenderer) < 0)
     {
         for (j = 0; j < TITLE_TEXT_NBR_OF_ELEMENTS; j++)
             SDL_DestroyTexture(pTextTextures[j]);
@@ -112,6 +117,54 @@ int create_rectangles_title(SDL_Texture *pTextTextures[],
                             SDL_Rect puzzleSizesRects[],
                             SDL_Rect &imgSDLRect)
 {
+    int i;
+
+    for (i = 0; i < TITLE_TEXT_NBR_OF_ELEMENTS; i++)
+    {
+        if (SDL_QueryTexture(pTextTextures[i], nullptr, nullptr, &textRects[i].w, &textRects[i].h) < 0)
+        {
+            error_message();
+            return -1;
+        }
+    }
+
+    for (i = 0; i <= PUZZLE_SIZE_MAX - PUZZLE_SIZE_MIN; i++)
+    {
+        if (SDL_QueryTexture(pPuzzleSizesTextures[i], nullptr, nullptr, &puzzleSizesRects[i].w, &puzzleSizesRects[i].h) < 0)
+        {
+            error_message();
+            return -1;
+        }
+    }
+
+    if (SDL_QueryTexture(pImgSDLTexture, nullptr, nullptr, &imgSDLRect.w, &imgSDLRect.h) < 0)
+    {
+        error_message();
+        return -1;
+    }
+    imgSDLRect.w = (3 * imgSDLRect.w) / 4;
+    imgSDLRect.h = (3 * imgSDLRect.h) / 4;
+
+    for (i = 0; i < TITLE_TEXT_NBR_OF_ELEMENTS; i++)
+    {
+        textRects[i].x = WINDOW_WIDTH / 2 - textRects[i].w / 2;
+
+        if (i != TITLE_PRESS_P)
+            textRects[i].y = 1.5 * i * textRects[i].h;
+        else
+            textRects[i].y = WINDOW_HEIGHT - 2 * textRects[i].h;
+    }
+
+    int space_width = WINDOW_WIDTH / (1 + PUZZLE_SIZES) - puzzleSizesRects[0].w;
+    for (i = 0; i < PUZZLE_SIZES; i++)
+    {
+        puzzleSizesRects[i].x = space_width + i * (puzzleSizesRects[0].w + space_width);
+        puzzleSizesRects[i].y = (WINDOW_HEIGHT + imgSDLRect.h) / 2 + 0.5 * puzzleSizesRects[i].h;
+    }
+
+    imgSDLRect.x = (WINDOW_WIDTH - imgSDLRect.w) / 2;
+    imgSDLRect.y = (WINDOW_HEIGHT - imgSDLRect.h) / 2;
+
     return 0;
 }
 
@@ -124,10 +177,34 @@ int update_screen_title(SDL_Renderer *pRenderer,
                         SDL_Rect &imgSDLRect,
                         SDL_Rect &selectionRect)
 {
+    int i;
     SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
     SDL_RenderClear(pRenderer);
-    SDL_SetRenderDrawColor(pRenderer, 31, 141, 59, 255);
-    SDL_RenderDrawLine(pRenderer, 17, 22, 504, 478);
+
+    for (i = 0; i < TITLE_TEXT_NBR_OF_ELEMENTS; i++)
+    {
+        if (SDL_RenderCopy(pRenderer, pTextTextures[i], nullptr, &textRects[i]) < 0)
+        {
+            error_message();
+            return -1;
+        }
+    }
+
+    for (i = 0; i < PUZZLE_SIZES; i++)
+    {
+        if (SDL_RenderCopy(pRenderer, pPuzzleSizesTextures[i], nullptr, &puzzleSizesRects[i]) < 0)
+        {
+            error_message();
+            return -1;
+        }
+    }
+
+    if (SDL_RenderCopy(pRenderer, pImgSDLTexture, nullptr, &imgSDLRect) < 0)
+    {
+        error_message();
+        return -1;
+    }
+
     SDL_RenderPresent(pRenderer);
     return 0;
 }
@@ -143,7 +220,7 @@ void destroy_textures_title(SDL_Texture *pTextTextures[],
     for (j = 0; j < TITLE_TEXT_NBR_OF_ELEMENTS; j++)
         SDL_DestroyTexture(pTextTextures[j]);
 
-    for (j = 0; j <= PUZZLE_SIZE_MAX - PUZZLE_SIZE_MIN; j++)
+    for (j = 0; j < PUZZLE_SIZES; j++)
         SDL_DestroyTexture(pPuzzleSizesTextures[j]);
 }
 
